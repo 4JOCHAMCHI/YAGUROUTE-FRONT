4da045c25481aa258654f82a9cb07aeaf9f321a4
Hong Kyuyoung <hky0715@gmail.com>
2024-08-16 11:30:54 +0900

    Feat: Add SignIn, SignUp view component

---
+++ yagu-route-front/src/views/SignIn.vue
@@ -0,0 +1,153 @@
+<script setup>
+import { ref } from 'vue'
+import { useRouter } from "vue-router";
+import axios from 'axios';
+
+const router = useRouter()
+
+const memberEmail = ref('')
+const memberPassword = ref('')
+const errorMessage = ref('')
+
+
+const signin = async() => {
+  if (memberEmail.value.trim() === '' || memberPassword.value.trim() === '') {
+    errorMessage.value = '이메일과 비밀번호를 모두 입력해주세요.'
+    return
+  }
+
+  try {
+    // 여기에 로그인 로직을 구현합니다.
+    console.log('로그인 시도:', memberEmail.value, memberPassword.value)
+    const response = await axios.post(`/rest_login`, {
+      username: memberEmail.value,
+      password: memberPassword.value
+    })
+
+    if (response.status == 200) {
+      router.push('dashboard') // 메인화면으로 리디렉션
+      errorMessage.value = '' // 에러 메시지 초기화
+    }
+  } catch(error) {
+    console.error('로그인 실패:', error)
+    alert("로그인에 실패했습니다.");
+  }
+}
+
+const redirectToNaverLogin = () => {
+  // 네이버 로그인 엔드포인트로 리디렉션
+  // window.location.href = '/oauth2/authorization/naver'
+  window.location.href = '/oauth2/naver'
+}
+
+const redirectToKakaoLogin = () => {
+  // 네이버 로그인 엔드포인트로 리디렉션
+  window.location.href = '/oauth2/authorization/kakao'
+}
+
+const redirectToGoogleLogin = () => {
+  // 네이버 로그인 엔드포인트로 리디렉션
+  window.location.href = '/oauth2/authorization/google'
+}
+</script>
+
+<template>
+  <v-container class="signin" >
+    <v-card class="mt-16">
+      <v-card-title class="py-0">
+        <h2 class="pb-4">로그인</h2>
+      </v-card-title>
+      <v-card-text>
+        <v-form @submit.prevent="signin" v-model="valid">
+          <v-text-field
+              v-model="memberEmail"
+              label="이메일"
+              prepend-icon="mdi-email"
+              type="email"
+              required
+          ></v-text-field>
+          <v-text-field
+              v-model="memberPassword"
+              label="비밀번호"
+              prepend-icon="mdi-lock"
+              type="password"
+              required
+          ></v-text-field>
+          <v-btn type="submit" color="#cf1c4e" size="x-large" class="text-h6" block>로그인</v-btn>
+
+        </v-form>
+        <v-alert v-if="errorMessage" type="error" class="mt-3">{{ errorMessage }}</v-alert>
+
+        <!-- 소셜 로그인 버튼 -->
+        <div class="social-login mt-8 ga-2">
+          <v-btn color="#03C75A" size="large" class="text-white" dark block @click="redirectToNaverLogin">
+            <span class="text-white text-h6 font-weight-bold mr-2">N</span>
+            네이버로 로그인
+          </v-btn>
+          <v-btn color="#FFEB00" size="large" dark block class="text-black mt-2" @click="redirectToKakaoLogin">
+            <v-icon left class="mr-2">mdi-chat</v-icon>
+            카카오로 로그인
+          </v-btn>
+          <v-btn color="#FFFFFF" size="large" dark block class="mt-2" @click="redirectToGoogleLogin">
+            <v-icon left class="mr-2" color="#DB4437">mdi-google</v-icon>
+            <span class="text-h7" style="color: #DB4437;">구글로 로그인</span>
+          </v-btn>
+        </div>
+
+        <!-- 소셜 로그인 버튼 가로 배열 -->
+        <!--  <div class="social-login mt-16 d-flex flex-row justify-space-around">
+            <v-btn
+              color="#03C75A"
+              icon=""
+              size="x-large"
+              @click="redirectToNaverLogin"
+            >
+              <span class="text-white text-h4 font-weight-bold">N</span>
+          </v-btn>
+          <v-btn
+            color="#FFEB00"
+            icon=""
+            size="x-large"
+            @click="redirectToKakaoLogin"
+          >
+            <v-icon size="30">mdi-chat</v-icon>
+          </v-btn>
+          <v-btn
+            color="#FFFFFF"
+            icon=""
+            size="x-large"
+            @click="redirectToGoogleLogin"
+          >
+            <v-icon size="30" color="#DB4437">mdi-google</v-icon>
+          </v-btn>
+        </div>
+        -->
+      </v-card-text>
+    </v-card>
+  </v-container>
+</template>
+
+<style scoped>
+.signin {
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
+
+.v-container {
+  max-width: 600px;
+}
+
+.v-card {
+  padding: 40px;
+}
+
+.v-btn {
+  margin-top: 20px;
+}
+
+.social-login {
+  display: flex;
+  flex-direction: column;
+}
+</style>
---
+++ yagu-route-front/src/views/SignUp.vue
@@ -0,0 +1,125 @@
+<script setup>
+import { ref, reactive, computed } from 'vue'
+import axios from "axios";
+import { useRouter } from "vue-router";
+
+const formData = reactive({
+  name: '',
+  email: '',
+  phone: '',
+  password: ''
+})
+
+const errorMessage = ref('')
+const showModal = ref(false)
+
+const router = useRouter() // Vue Router 인스턴스 생성
+
+const isFormValid = computed(() => {
+  return formData.name !== '' &&
+      formData.email !== '' &&
+      formData.phone !== '' &&
+      formData.password !== ''
+})
+
+const signup = async () => {
+  if (isFormValid.value) {
+    try {
+      let url = `/member/signup`;
+      const response = await axios.post(url, {
+        memberName: formData.name,
+        memberEmail: formData.email,
+        memberPhone: formData.phone,
+        memberPassword: formData.password, // 비밀번호를 함께 전송하는 경우
+      });
+
+      console.log('회원가입 성공:', response.data)
+      errorMessage.value = '' // 에러 메시지 초기화
+      showModal.value = true
+
+      setTimeout(() => {
+        showModal.value = false
+        router.push('/') // 메인화면으로 리디렉션
+      }, 2000);
+    } catch (error) {
+      console.error('회원가입 실패:', error)
+      alert("회원가입 중 오류가 발생했습니다. 다시 시도해주세요.");
+    }
+  } else {
+    errorMessage.value = '모든 필드를 채워주세요.'
+  }
+}
+
+</script>
+
+<template>
+  <v-container class="register">
+    <v-card class="mt-16">
+      <v-card-title class="py-0">
+        <h2 class="pb-4">회원가입</h2>
+      </v-card-title>
+      <v-card-text>
+        <v-form @submit.prevent="signup" v-model="valid">
+          <v-text-field
+              v-model="formData.name"
+              label="이름"
+              prepend-icon="mdi-account"
+              type="text"
+              required
+          ></v-text-field>
+          <v-text-field
+              v-model="formData.email"
+              label="이메일"
+              prepend-icon="mdi-email"
+              type="email"
+              required
+          ></v-text-field>
+          <v-text-field
+              v-model="formData.phone"
+              label="전화번호"
+              prepend-icon="mdi-phone"
+              type="tel"
+              required
+          ></v-text-field>
+          <v-text-field
+              v-model="formData.password"
+              label="비밀번호"
+              prepend-icon="mdi-lock"
+              type="password"
+              required
+          ></v-text-field>
+          <v-btn type="submit" color="#cf1c4e" size="x-large" class="text-h6" block>가입하기</v-btn>
+        </v-form>
+        <v-alert v-if="errorMessage" type="error" class="mt-3">{{ errorMessage }}</v-alert>
+      </v-card-text>
+    </v-card>
+
+    <!-- 환영 모달 -->
+    <v-dialog v-model="showModal" max-width="600">
+      <v-card class="d-flex">
+        <v-card-title class="text-h5">환영합니다!</v-card-title>
+        <v-card-text>회원가입이 성공적으로 완료되었습니다.</v-card-text>
+      </v-card>
+    </v-dialog>
+  </v-container>
+</template>
+
+<style scoped>
+.register {
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
+
+.v-container {
+  max-width: 600px;
+}
+
+.v-card {
+  padding: 40px;
+}
+
+.v-btn {
+  margin-top: 20px;
+}
+</style>
