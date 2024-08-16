9bb3a5f6d5d1aaf7dfe4ad17b2b504d0e617bc2f
goeunpark123 <goeunpark21@gmail.com>
2024-08-16 11:18:41 +0900

    Feat: Connect seat booking screen

--- yagu-route-front/src/views/TicketBookingView.vue
+++ yagu-route-front/src/views/TicketBookingView.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="seat-reservation">
-    <h1>좌석 예매</h1>
+    <h1>경기 정보</h1>
     <div v-if="loading">좌석 정보를 불러오는 중...</div>
     <div v-else class="reservation-layout">
       <div class="seat-grid-container">
@@ -21,7 +21,7 @@
           <p v-if="selectedSeat">{{ selectedSeat.seatNum }}</p>
           <p v-else>좌석을 선택해주세요.</p>
         </div>
-        <button @click="reserveSeat(11, selectedSeat?.seatId)" :disabled="!selectedSeat">예매하기</button>
+        <button @click="reserveSeat(id, selectedSeat.seatId)" :disabled="!selectedSeat">예매하기</button>
       </div>
     </div>
   </div>
@@ -31,6 +31,12 @@
 import { ref, computed, onMounted } from 'vue'
 import axios from "axios";
 
+const props = defineProps({
+  gameId: 0
+})
+
+const id = ref(props.gameId);
+
 const seats = ref([])
 const occupiedSeats = ref([])
 const selectedSeat = ref(null)
@@ -39,9 +45,6 @@ const loading = ref(true)
 const gridStyle = computed(() => {
   const seatList = seats.value;
   const maxCol = seatList.length > 0 ? Math.max(...seatList.map(seat => seat.seatCol)) : 0
-
-  console.log(seats.value);
-  console.log(maxCol);
 
   return {
     display: 'grid',
@@ -64,7 +67,6 @@ const reserveSeat = async (gameId, seatId) => {
       let url = `http://localhost:8080/ticket/1/${gameId}/${seatId}`;
       const response = await axios.post(url)
 
-      console.log(response.data.message);
       alert(selectedSeat.value.seatNum + "번 " + response.data.message);
 
     } catch (error) {
@@ -77,6 +79,8 @@ const reserveSeat = async (gameId, seatId) => {
   }
 }
 
+
+
 // 전체 좌석 정보를 가져오는 함수
 const fetchAllSeats = async (gameId) => {
   try {
@@ -84,6 +88,7 @@ const fetchAllSeats = async (gameId) => {
     const response = await axios.get(url);
 
     if (response.status === 200) {
+      console.log(response.data);
       return response.data.result.allSeats;
     } else {
       console.error("Error: Response status is not 200");
@@ -115,8 +120,8 @@ const fetchOccupiedSeats = async (gameId) => {
 onMounted(async () => {
   try {
     const [allSeats, unavailableSeats] = await Promise.all([
-      fetchAllSeats(11),
-      fetchOccupiedSeats(11)
+      fetchAllSeats(id.value),
+      fetchOccupiedSeats(id.value)
     ])
 
     seats.value = allSeats;
@@ -142,32 +147,43 @@ h1 {
 }
 
 h2 {
-  margin-bottom: 40px;
+  font-size: 25px;
+  margin-bottom: 20px;
 }
 
 .reservation-layout {
   display: grid;
-  grid-template-columns: 3fr 1fr;
+  grid-template-columns: 3.2fr 0.8fr;
   gap: 20px;
+  height: 450px; /* 고정 높이 설정 */
 }
 
 .seat-grid-container {
-  overflow-x: auto;
-}
-
-.seat-grid {
-  display: grid;
-  gap: 5px;
-}
-
-.selected-seat-container {
-  padding: 20px;
+  padding: 5px 0;
+  overflow: auto;
+  height: 100%;
   border: 1px solid #ccc;
   border-radius: 5px;
 }
 
+.seat-grid {
+  display: grid;
+  gap: 3px;
+  padding: 15px;
+}
+
+.selected-seat-container {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  padding: 15px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  height: 100%;
+}
+
 .seat {
-  padding: 10px;
+  padding: 3px;
   border: 1px solid #ccc;
   cursor: pointer;
   text-align: center;
@@ -185,17 +201,17 @@ h2 {
 }
 
 .selected-seat {
-  margin-top: 20px;
+  flex-grow: 1;
 }
 
 button {
-  margin-top: 300px;
-  padding: 15px 20px;
+  padding: 10px 20px;
   background-color: #cf1c4e;
   color: white;
   border: none;
   cursor: pointer;
   width: 100%;
+  margin-top: 20px;
 }
 
 button:disabled {
