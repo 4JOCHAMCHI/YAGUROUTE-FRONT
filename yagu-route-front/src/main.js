567117adc4ec6e317f94f6f3bb403b7d82321573
Hong Kyuyoung <hky0715@gmail.com>
2024-08-16 11:36:12 +0900

    Feat: Add vuetify import

--- yagu-route-front/src/main.js
+++ yagu-route-front/src/main.js
@@ -2,12 +2,12 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import axios from 'axios'
 import router from '../src/router/router.js'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import vuetify from './plugins/vuetify.js'
 
 
 const app = createApp(App);
 
 app.use(router);
+app.use(vuetify);
 app.mount('#app');
 app.provide('$axios', axios);
